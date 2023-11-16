ccdb30a6d1fe3a3b7a9c4bfd06c05b104519ba0e
David Alcalde <dalcalde@simplai.es>
2023-11-16 11:12:50 +0100

    Update check_db.sh

--- check_db.sh
+++ check_db.sh
@@ -5,8 +5,9 @@ until pg_isready -h localhost -p 5432 -U $POSTGRESQL_USER; do
 done
 
 if psql -h localhost -p 5432 -U $POSTGRESQL_USER -d $POSTGRESQL_DATABASE -c "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public';" | grep -q '0'; then
-  echo "Relationship does not exist. Running your command..."
+  echo "Relationship does not exist. Running creation of dabatase..."
 
+  psql -U admin $POSTGRESQL_USER $POSTGRESQL_DATABASE << dump.sql
 
 else
   echo "Relationship already exists. Skipping command."
